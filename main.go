c649054510ec795002cb673ce1f42e98796e3a27
Wesley Willians <wesleywillians@gmail.com>
2021-09-23 19:54:56 -0400

    teste

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World@@@@@@"))
+		w.Write([]byte("Hello World@@@@@@!"))
 	})
 	http.ListenAndServe(":80", nil)
 }
